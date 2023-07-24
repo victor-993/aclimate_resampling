424d41e8c982bfe77633d549720d1b6f848e8967
stevensotelo <stevenbetancurt@hotmail.com>
2023-07-24 11:45:15 -0500

    working in previous month

--- src/download_data.py
+++ src/download_data.py
@@ -34,6 +34,7 @@ class DownloadProgressBar(tqdm):
 
 class DownloadData():
 
+    # start_date: start date to download.
     # coords: North, West, South, East
     def __init__(self,start_date,country,path,region,cores = 1,force = False):
         self.start_date = start_date
@@ -42,7 +43,8 @@ class DownloadData():
         self.region = region
         self.cores = cores
         self.force = force
-        
+        self.end_date = (self.start_date + pd.DateOffset(months=1)) - pd.DateOffset(days=1)
+
         pass
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Function which creates a folder. It checks if the folders exist before
@@ -63,20 +65,18 @@ class DownloadData():
     # Function to download chirps data
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # save_path: path to save raster files.
-    # start_date: start date to download.
-    # end_date: end date to download.
     # year_to: resampling year.
     # cores: # cores to use in parallel.
     # force: If you want to force to execute the process
     # OUTPUT: save rasters layers.
-    def download_data_chirp(self,save_path, start_date, end_date, year_to):
+    def download_data_chirp(self,save_path, year_to):
         # Create folder for data
         save_path_chirp = os.path.join(save_path,"chirps")
         self.mkdir(save_path_chirp)
-        
+
         # Calculate dates to download data
-        dates = [start_date + timedelta(days=x) for x in range((end_date - start_date).days + 1)]
-        
+        dates = [self.start_date + timedelta(days=x) for x in range((self.end_date - self.start_date).days + 1)]
+
         # Creating a list of all files that should be downloaded
         urls = [f"http://data.chc.ucsb.edu/products/CHIRP/daily/{year_to}/chirp.{date.strftime('%Y.%m.%d')}.tif" for date in dates]
         files = [os.path.basename(url) for url in urls]
@@ -91,12 +91,10 @@ class DownloadData():
     # Function to download ERA 5 data
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # save_path: path to save raster files.
-    # start_date: start date to download.
-    # end_date: end date to download.
     # variables: List of variables to download. by default
     # force: If you want to force to execute the process
     # OUTPUT: save rasters layers.
-    def download_era5_data(self,save_path, start_date, end_date, variables=["t_max","t_min","sol_rad"]):
+    def download_era5_data(self,save_path, variables=["t_max","t_min","sol_rad"]):
         # Define the variables classes and their parameters for the CDSAPI
         enum_variables ={
                             "t_max":{"name":"2m_temperature",
@@ -112,9 +110,9 @@ class DownloadData():
         self.mkdir(save_path_era5)
 
         # Calculate dates to download data
-        year = start_date.strftime("%Y")
-        month = start_date.strftime("%m")
-        days = [(start_date + timedelta(days=x)).strftime("%d") for x in range((end_date - start_date).days + 1)]
+        year = self.start_date.strftime("%Y")
+        month = self.start_date.strftime("%m")
+        days = [(self.start_date + timedelta(days=x)).strftime("%d") for x in range((self.end_date - self.start_date).days + 1)]
 
         # Process for each variable that should be downloaded
         for v in variables:
@@ -149,56 +147,94 @@ class DownloadData():
                 zObject.extractall(path=save_path_era5_data)
             print("\tExtracted!")
 
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # Function to extract data from rasters
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # dir_path: path where it should take rasters files.
+    # var: The name of the variable
+    # locations: Dataframe with the stations
+    # ext: Extension files
+    # date_start: Position into the filename where the date starts
+    # date_end: Position into the filename where the date ends
+    # date_format: Format in which we can find the date in the filename
+    # OUTPUT: list with values extracted by variable, date, and station.
+    def extract_values(self,dir_path,var,locations, ext, date_start,date_end,date_format):
+        files = [f for f in os.listdir(dir_path) if f.endswith(ext)]
+        data = []
 
-
+        # Loop for each daily file
+        for file in tqdm(files,desc="Extracting " + var):
+            file_path = os.path.join(dir_path, file)
+            with rasterio.open(file_path) as src:
+                # transform = src.transform
+                # Loop for each location
+                for index,location in locations.iterrows():
+                    #col, row = ~transform * (location['lon'], location['lat'])
+                    row, col = src.index(location['lon'], location['lat'])
+                    value = src.read(1)[row, col]
+                    date_str = file[date_start:date_end]
+                    date = datetime.datetime.strptime(date_str, date_format)
+                    data.append({'ws':location['ws'],
+                                'day':date.day,
+                                'month':date.month,
+                                'year': date.year,
+                                var: value})
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Function to extract Chirp data
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # save_path:  rasters path
-    # locations: list of coordinates for each location that we want to extract.
+    # locations: Dataframe with coordinates for each location that we want to extract.
     # OUTPUT: This return resampling scenaries join with satellite data.
     def extract_chirp_data(self,save_path, locations):
-        files = [f for f in os.listdir(save_path) if f.endswith('.tif')]
-        data = []
-        
-        for file in files:
-            file_path = os.path.join(save_path, file)
-            with rasterio.open(file_path) as dataset:
-                for location in locations:
-                    row, col = dataset.index(location['lon'], location['lat'])
-                    value = dataset.read(1)[row, col]
-                    date_str = file[-10:-4]
-                    date = datetime.datetime.strptime(date_str, '%Y%m%d')
-                    data.append({'date': date, 'prec': value})
-        
+        dir_path = os.path.join(save_path,"chirps")
+        data = self.extract_values(dir_path,'prec',locations,'.tif',-14,-4,'%Y.%m.%d')
         df = pd.DataFrame(data)
-        df['day'] = df['date'].dt.day
-        df['month'] = df['date'].dt.month
-        df['year'] = df['date'].dt.year
-        df = df[['day', 'month', 'year', 'prec']]
-        
         return df
 
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # Function to extract ERA 5 data
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # save_path:  rasters path
+    # locations: Dataframe with coordinates for each location that we want to extract.
+    # variables: List of variables to be extracted. by default
+    # OUTPUT: This return resampling scenaries join with satellite data.
+    def extract_era5_data(self,save_path, locations,variables=["t_max","t_min","sol_rad"]):
+        df = pd.DataFrame()
+        for v in variables:
+            dir_path = os.path.join(save_path,"era5",v)
+            data = self.extract_values(dir_path,v,locations,'.nc',-22,-14,'%Y%m%d')
+            df_tmp = pd.DataFrame(data)
+            if df.shape[0] == 0:
+                df = df_tmp.copy()
+            else:
+                df = pd.merge(df,df_tmp,how='left',on=['ws','day','month','year'])
+        return df
 
-    ## Funcion para crear mes previo promedio a partir de los datos historicos
-    ## Mes previo  = month(Sys.month) - 1
+    def extract_climatology(self,save_path,locations):
+        df = pd.DataFrame()
+        # Loop for each location
+        for index,location in tqdm(locations.iterrows(),desc="Calculating climatology"):
+            file_path = os.path.join(save_path,location["ws"] + ".csv")
+            df_tmp = pd.read_csv(file_path)
+            df_tmp = df_tmp.groupby(['day', 'month']).agg({
+                            't_max': 'mean',
+                            't_min': 'mean',
+                            'prec': 'median',
+                            'sol_rad': 'mean'
+                    }).reset_index()
+            df_tmp = df_tmp.loc[df_tmp['month'] == self.start_date.month,:]
+            df_tmp['ws'] = location["ws"]
 
-    ###day month  year t_max t_min  prec sol_rad
-    def clim_extract(self,wth_data, ini_date):
-        wth_data = wth_data.groupby(['day', 'month']).agg({
-            't_max': 'mean',
-            't_min': 'mean',
-            'prec': 'median',
-            'sol_rad': 'mean'
-        }).reset_index()
-        
-        wth_data['year'] = ini_date.year
-        wth_data = wth_data[wth_data['month'] == ini_date.month]
-        wth_data = wth_data[['day', 'month', 'year', 't_max', 't_min', 'prec', 'sol_rad']]
-        
-        return wth_data
+            if df.shape[0] == 0:
+                df = df_tmp.copy()
+            else:
+                df = pd.merge(df,df_tmp,how='left',on=['ws','year','month','day'])
 
+        df["year"] = self.start_date.year
+        df = df[['ws','day', 'month', 'year', 'prec','t_max', 't_min', 'sol_rad']]
+
+        return df
 
     # Funcion para unir bases de datos y extraer escenarios finales
     # Escenaries <- all_wth_data$Escenaries[[1]]#$data[[1]]$data[[1]] 
@@ -221,34 +257,28 @@ class DownloadData():
         
         return Escenaries
 
-    # Function to save scenarios
-    def write_scenarios(current_index=None):
-        current_index = 1 if current_index is None else current_index
-
-        # Load wth_escenaries from pickle file if it exists
-        if "wth_escenaries" in locals():
-            with open(f"/forecast/workdir/{currentCountry}_wth_scenaries.pkl", "rb") as f:
-                wth_escenaries = pickle.load(f)
-        else:
-            wth_escenaries = pd.read_pickle(f"/forecast/workdir/{currentCountry}_wth_scenaries.pkl")
-
-        length_wth = len(wth_escenaries)
-        for current_index in range(current_index, length_wth, 100):
-            sub_wth_escenaries = wth_escenaries.iloc[current_index:min(current_index + 99, length_wth)]
-
-            for id_, wth_final in zip(sub_wth_escenaries['id'], sub_wth_escenaries['wth_final']):
-                function_to_save(id_, wth_final, path_output)
-
-            print(f"{current_index}-{min(current_index + 99, length_wth)} scenarios written")
+    def write_outputs(self,save_path,locations,data,climatology,variables=['prec','t_max','t_min','sol_rad']):
+        cols_date = ['day','month','year']
+        cols_total = cols_date + variables
+        for index,location in tqdm(locations.iterrows(),desc="Writing scenarios"):
+            files = glob.glob(os.path.join(save_path, '*'))
+            for f in files:
+                df_tmp = pd.read_csv(f)
+                df_data = data.loc[data["ws"] == location["ws"],cols_total]
+                # We have to use climatology
+                if df_data.shape[0] == 0:
+                    df_data = climatology.loc[climatology["ws"] == location["ws"],cols_total]
+                df_data = df_data.append(df_tmp,ignore_index=True)
+                df_data.write_csv(f)
 
 
-    def run(self,start_date):
+
+    def run(self):
         ## up 2021, jre: Se estandariza el uso de formato fecha, se requiere paquete lubridate
         print("Calculating dates for the process")
-        end_date = (self.start_date + pd.DateOffset(months=1)) - pd.DateOffset(days=1)
         year_to = self.start_date.year
         month_to = self.start_date.month
-        print("Init:",self.start_date,"End:",end_date,"Year:",year_to,"Month:",month_to)
+        print("Init:",self.start_date,"End:",self.end_date,"Year:",year_to,"Month:",month_to)
 
         # Validating folder
         print("Validating folders")
@@ -257,6 +287,7 @@ class DownloadData():
         path_outputs = os.path.join(path_country,"outputs")
         path_daily = os.path.join(path_inputs,"prediccionClimatica","dailyData")
         path_resampling = os.path.join(path_outputs,"prediccionClimatica","resampling")
+        path_daily_data = os.path.join(path_inputs,"prediccionClimatica","dailyData") # It is not included because we will create after
         path_daily_downloaded = os.path.join(path_inputs,"prediccionClimatica","daily_downloaded") # It is not included because we will create after
 
         folders = [path_country,path_inputs,path_outputs,path_daily,path_resampling]
@@ -267,21 +298,64 @@ class DownloadData():
                 missing_files.append(folder)
 
         if len(missing_files) > 0:
-            print("Directories don't exist",missing_files)
+            print("ERROR Directories don't exist",missing_files)
         else:
             self.mkdir(path_daily_downloaded)
 
             # Download Chirps data
             print("CHIRPS data started!")
-            self.download_data_chirp(path_daily_downloaded, start_date, end_date, year_to)
+            self.download_data_chirp(path_daily_downloaded, year_to)
             print("CHIRPS data downloaded!")
 
             # Download ERA 5 data
             print("ERA 5 data started!")
-            self.download_era5_data(path_daily_downloaded, start_date, end_date)
+            self.download_era5_data(path_daily_downloaded)
             print("ERA 5 data downloaded!")
-            
 
+            # Final list of stations to be processed
+            print("Listing final stations")
+            errors = 0
+            df_ws = pd.DataFrame(columns=["ws","lat","lon","message"])
+            df_ws["ws"] =[w.split(os.path.sep)[-1] for w in glob.glob(os.path.join(path_resampling, '*'))]
+            for index,row in df_ws.iterrows():
+                try:
+                    df_tmp = pd.read_csv(os.path.join(path_daily_data,row["ws"] + "_coords.csv"))
+                    df_ws.at[index,"lat"],df_ws.at[index,"lon"] = df_tmp.at[0,"lat"],df_tmp.at[0,"lon"]
+                except Exception:
+                    errors += 1
+                    df_ws.at[index,"message"] = "ERROR with coordinates"
+
+            if errors > 0:
+                print("WARNING: Stations with problems",df_ws.loc[df_ws["message"].isna() == False,:])
+
+            df_data = pd.DataFrame()
+
+            print("Extracting CHIRPS data")
+            df_data_chirps = self.extract_chirp_data(path_daily_downloaded,df_ws)
+            print("Extracted CHIRPS data")
+
+            print("Extracting ERA 5 data")
+            df_data_era5 = self.extract_era5_data(path_daily_downloaded,df_ws)
+            print("Extracted ERA 5 data")
+
+            print("Merging CHIRPS and ERA 5")
+            df_data = pd.merge(df_data_chirps,df_data_era5,how='outer',on=['ws','day','month','year'])
+            print("Merged CHIRPS and ERA 5")
+
+            ########### Code to validate
+
+            print("Extracting Climatology data")
+            df_data_climatology = self.extract_climatology(path_daily_data,df_ws)
+            print("Extracted Climatology data")
+
+            print("Writing scenarios")
+            self.write_outputs(path_resampling,df_ws,df_data,df_data_climatology)
+            print("Finished")
+
+
+
+
+            
             # Initialize wth_escenaries
             #wth_escenaries = Resam.copy()
             #wth_escenaries['chirp_data'] = wth_escenaries['coord'].apply(lambda x: self.extract_chirp_data(path_chirp, x))
@@ -298,7 +372,7 @@ class DownloadData():
             #    pickle.dump(wth_escenaries, f)
 
             #write_scenarios()
-                
+
             # Remove chirp files
             #files = glob.glob(os.path.join(path_output, "*.tif"))
             #for file in files:
--- src/main.py
+++ src/main.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     print("Setting global parameters")
     start_date = (datetime.date.today() - pd.DateOffset(months=1)).replace(day=1)
     country = "ETHIOPIA"
+    #country = "TANZANIA"
     path = "D:\\CIAT\\Code\\USAID\\aclimate_resampling\\data\\"
-    region = [14,32,3,48]
+    region = [14,32,3,48] # North, West, South, East
+    ##region = [0,28,-12,41] # North, West, South, East
     cores = 2
     dd = DownloadData(start_date,country,path,region,cores=cores)
     dd.run()
