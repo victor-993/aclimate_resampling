375577241f78cc07ab89c7fad370ec4f080d052c
stevensotelo <stevenbetancurt@hotmail.com>
2023-07-25 11:18:58 -0500

    added setup

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aclimate_resampling",
+    version='v1.0.0',
+    author="stevensotelo",
+    author_email="h.sotelo@cgiar.com",
+    description="Resampling module",
+    url="https://github.com/CIAT-DAPA/aclimate_resampling",
+    download_url="https://github.com/CIAT-DAPA/aclimate_resampling",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='resampling aclimate',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "mongoengine==0.26.0"
+    ]
+)
+"""
 import datetime
 
 import pandas as pd
@@ -18,3 +41,4 @@ if __name__ == "__main__":
     #dd = DownloadData(start_date,country,path,region,cores=cores)
     dd = DownloadData(start_date,country,path,cores=cores)
     dd.run()
+"""
