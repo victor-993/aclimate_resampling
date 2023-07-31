5f29d068804059722e97e98d2b475ff3d075a87b
stevensotelo <stevenbetancurt@hotmail.com>
2023-07-31 14:33:12 -0500

    added case uses for download era 5 and added two cases for leap year 2020

--- test/test_complete_data.py
+++ test/test_complete_data.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import shutil
+import glob
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -26,7 +27,11 @@ class TestCompleteData(unittest.TestCase):
         self.chirps_file_path = os.path.join(self.chirps_path, self.chirps_file_name)
         self.chirps_file_path_compressed = os.path.join(self.chirps_path, self.chirps_url_name)
         self.url = f"http://data.chc.ucsb.edu/products/CHIRP/daily/{str(self.start_date.year)}/{self.chirps_url_name}"
+        self.era5_path = os.path.join(self.daily_downloaded_path, "era5")
         os.makedirs(self.chirps_path, exist_ok=True)
+
+        # Leap year
+        self.start_date_leapyear = datetime(2020, 2, 1)
 
     def tearDown(self):
         # Clean up the temporary test directory and its contents after each test
@@ -126,6 +131,66 @@ class TestCompleteData(unittest.TestCase):
             self.assertTrue(os.path.exists(file_path))
             self.assertEqual(os.path.getsize(file_path), 9)  # Size of the "Mock data"
 
+    def test_download_data_chirp_leapyear(self):
+        # Test downloading chirp data for a leap year
+        complete_data = CompleteData(start_date=self.start_date_leapyear, country=self.country, path=self.root_data, cores=self.cores)
+
+        # Perform the download
+        complete_data.download_data_chirp(self.daily_downloaded_path, year_to=self.start_date_leapyear.year)
+
+        # Check if the chirp data files were downloaded and stored in the correct location
+        dates = [self.start_date_leapyear + timedelta(days=x) for x in range((self.end_date - self.start_date_leapyear).days + 1)]
+        expected_files = [f"chirp.{date.strftime('%Y.%m.%d')}.tif" for date in dates]
+        for file in expected_files:
+            file_path = os.path.join(self.chirps_path, file)
+            self.assertTrue(os.path.exists(file_path))
+        
+        transformed_files = glob.glob(os.path.join(self.chirps_path, '*.tif'))
+        self.assertEqual(len(transformed_files), 29)  # 29 days of data downloaded for each variable
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # TEST DOWNLOAD ERA 5
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    def test_download_era5_data_single_variable(self):
+        # Test downloading era5 data for a single variable
+        complete_data = CompleteData(start_date=self.start_date, country=self.country, path=self.root_data, cores=self.cores)
+
+        # Perform the download for a single variable (t_max)
+        complete_data.download_era5_data(self.daily_downloaded_path, variables=["t_max"])
+
+        # Check if the era5 data files were downloaded, extracted, and transformed
+        variable_path = os.path.join(self.era5_path, "t_max")
+        self.assertTrue(os.path.exists(variable_path))
+        transformed_files = glob.glob(os.path.join(variable_path, '*.tif'))
+        self.assertEqual(len(transformed_files), 30)  # 30 days of data downloaded
+
+    def test_download_era5_data_multiple_variables(self):
+        # Test downloading era5 data for multiple variables (t_max, t_min, sol_rad)
+        complete_data = CompleteData(start_date=self.start_date, country=self.country, path=self.root_data, cores=self.cores)
+
+        # Perform the download for multiple variables
+        complete_data.download_era5_data(self.daily_downloaded_path, variables=["t_max", "t_min", "sol_rad"])
+
+        # Check if the era5 data files were downloaded, extracted, and transformed for all variables
+        variables = ["t_max", "t_min", "sol_rad"]
+        for variable in variables:
+            variable_path = os.path.join(self.era5_path, variable)
+            self.assertTrue(os.path.exists(variable_path))
+            transformed_files = glob.glob(os.path.join(variable_path, '*.tif'))
+            self.assertEqual(len(transformed_files), 30)  # 3 days of data downloaded for each variable
+
+    def test_download_era5_data_single_variable_leapyear(self):
+        # Test downloading era5 data for a single variable
+        complete_data = CompleteData(start_date=self.start_date, country=self.country, path=self.root_data, cores=self.cores)
+
+        # Perform the download for a single variable (t_max)
+        complete_data.download_era5_data(self.daily_downloaded_path, variables=["t_max"])
+
+        # Check if the era5 data files were downloaded, extracted, and transformed
+        variable_path = os.path.join(self.era5_path, "t_max")
+        self.assertTrue(os.path.exists(variable_path))
+        transformed_files = glob.glob(os.path.join(variable_path, '*.tif'))
+        self.assertEqual(len(transformed_files), 29)  # 30 days of data downloaded
 
 if __name__ == '__main__':
     unittest.main()
